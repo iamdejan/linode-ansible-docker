70fa092759f72c1f03d3c9754acc816df5ec4e30
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-01-10 17:04:07 +0700

    Install Ansible through SSH

--- main.tf
+++ main.tf
@@ -24,3 +24,23 @@ resource "linode_instance" "ansible_instance" {
   root_pass = var.root_password
 }
 
+resource "null_resource" "install_ansible" {
+  depends_on = [linode_instance.ansible_instance]
+
+  triggers = {
+    public_ip = linode_instance.ansible_instance.ip_address
+  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    password = var.root_password
+    host = linode_instance.ansible_instance.ip_address
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt update && apt install ansible -y && mkdir -p /etc/ansible && touch /etc/ansible/hosts"
+    ]
+  }
+}
